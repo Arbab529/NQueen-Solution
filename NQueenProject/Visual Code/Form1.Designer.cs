27cad920a1ed7fb8dcf47d9e280f7c6278cc6759
ARBAB ANJUM <arbabanjum0347@gmail.com>
2019-12-09 23:15:24 +0500

    DSA project

---
+++ NQueenProject/Visual Code/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Visual_Code
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(819, 538);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 57);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSize
+            // 
+            this.txtSize.Font = new System.Drawing.Font("Minion Pro", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSize.Location = new System.Drawing.Point(865, 472);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(101, 36);
+            this.txtSize.TabIndex = 1;
+            this.txtSize.TextChanged += new System.EventHandler(this.txtSize_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(813, 476);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N = ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(813, 511);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Generate Table";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(817, 608);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 677);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ NQueenProject/Visual Code/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Visual_Code
+{
+    public partial class Form1 : Form
+    {
+        int first=0;
+        bool refresh = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            int L = int.Parse(txtSize.Text);
+                int x = 15, y = 15;
+                N = L;
+            int[,] arr= new int[L,L];
+           
+            if (!theBoardSolver(arr, 0))
+            {
+                Console.WriteLine("Solution not Possible .");
+            }
+   
+            for (int i = 0; i < L; i++)
+            {
+                for (int j = 0; j < L; j++)
+                {
+                   
+                    Button btn = new Button();
+                    btn.Text = "Queen"+" "+i+j;
+                    btn.Width = 60;
+                    btn.Height = 60;
+                    btn.Left = x;
+                    btn.Top = y;
+                    btn.Name = btn + "" + i;
+                    this.Controls.Add(btn);
+                    x += 75;
+                    if (arr[i,j]==1)
+                    {
+                        btn.BackColor = Color.Red;
+                    }
+                   
+                }
+                x = 15;
+                y += 75;
+            }
+            //RefreshData(arr);
+            first++;
+        }
+        static int N;
+       
+        static Boolean toPlaceOrNotToPlace(int[,] board, int row, int col)
+        {
+            int i, j;
+            for (i = 0; i < col; i++)
+            {
+                if (board[row, i] == 1) return false;
+            }
+            for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            {
+                if (board[i, j] == 1) return false;
+            }
+            for (i = row, j = col; j >= 0 && i < N; i++, j--)
+            {
+                if (board[i, j] == 1) return false;
+            }
+            return true;
+        }
+        static Boolean theBoardSolver(int[,] board, int col)
+        {
+            if (col >= N) return true;
+            for (int i = 0; i < N; i++)
+            {
+                if (toPlaceOrNotToPlace(board, i, col))
+                {
+                    board[i, col] = 1;
+                    if (theBoardSolver(board, col + 1)) return true;
+                    board[i, col] = 0;
+                }
+            }
+            return false;
+        }
+        //private void RefreshData(int[,] array)
+        //{
+        //    for (int i = 0; i < array.GetLength(0); i++)
+        //    {
+        //        for (int j = 0; j < array.GetLength(1); j++)
+        //        {
+
+        //        }
+        //}
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+                refresh = true;
+                txtSize.Clear();
+                Form1 f = new Form1();
+                f.Show();
+                this.Hide();
+        }
+
+        private void txtSize_TextChanged(object sender, EventArgs e)
+        {
+            //if (first > 0 && refresh==false)
+            //{
+            //    refresh = true;
+            //    txtSize.Clear();
+            //    Form1 f = new Form1();
+            //    f.Show();
+            //    this.Hide();
+
+            //}
+        }
+
+    }
+}
